ada2cb7fb19f75b7ff4b6f3cfd8a626c048ee738
Sébastien Han <seb@redhat.com>
2018-02-19 10:13:06 +0100

    add ceph_crush module
    This module allows us to create Ceph CRUSH hierarchy. The module works with
    hostvars from individual OSD hosts.
    Here is an example of the expected configuration in the inventory file:
    
    [osds]
    ceph-osd-01 osd_crush_location="{ 'root': 'mon-roottt', 'rack': 'mon-rackkkk', 'pod': 'monpod', 'host': 'localhost' }"  # valid case
    
    Then, if create_crush_tree is enabled the module will create the
    appropriate CRUSH buckets and their types in Ceph.
    
    Some pre-requesites:
    
    * a 'host' bucket must be defined
    * at least two buckets must be defined (this includes the 'host')
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

---
+++ library/ceph_crush.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+import datetime
+
+from ansible.module_utils.basic import AnsibleModule
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.0',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: ceph_crush
+
+author: Sebastien Han <seb@redhat.com>
+
+short_description: Create Ceph CRUSH hierarchy
+
+description:
+    - By using the hostvar variable 'osd_crush_location'
+    ceph_crush creates buckets and places them in the right CRUSH hierarchy
+'''
+
+EXAMPLES = '''
+- name: configure crush hierarchy
+  ceph_crush:
+    cluster: "{{ cluster }}"
+    location: "{{ hostvars[item]['osd_crush_location'] }}"
+    containerized: "{{ docker_exec_cmd }}"
+  with_items: "{{ groups[osd_group_name] }}"
+  when:
+    - crush_rule_config
+'''
+
+
+def fatal(message, module):
+    '''
+    Report a fatal error and exit
+    '''
+    if module:
+        module.fail_json(msg=message, rc=1)
+    else:
+        raise(Exception(message))
+
+
+def generate_cmd(cluster, subcommand, bucket, bucket_type, containerized=None):
+    '''
+    Generate command line to execute
+    '''
+    cmd = [
+        'ceph',
+        '--cluster',
+        cluster,
+        'osd',
+        'crush',
+        subcommand,
+        bucket,
+        bucket_type,
+    ]
+    if containerized:
+        for piece in reversed(containerized.split()):
+            cmd.insert(0, piece)
+    return cmd
+
+
+def sort_osd_crush_location(location, module):
+    '''
+    Sort osd_crush_location dict
+    '''
+    len(location) < 2 and fatal("You must specify at least 2 buckets.", module)
+    [item for item in location if item[0] == "host"] or fatal("You must specify a 'host' bucket.", module)
+    try:
+        crush_bucket_types = [
+            "host",
+            "chassis",
+            "rack",
+            "row",
+            "pdu",
+            "pod",
+            "room",
+            "datacenter",
+            "region",
+            "root",
+        ]
+        return sorted(location, key=lambda crush: crush_bucket_types.index(crush[0]))
+    except ValueError as error:
+        fatal("{} is not a valid CRUSH bucket, valid bucket types are {}".format(error.args[0].split()[0], crush_bucket_types), module)
+
+
+def create_and_move_buckets_list(cluster, location, containerized=None):
+    '''
+    Creates Ceph CRUSH buckets and arrange the hierarchy
+    '''
+    previous_bucket = None
+    cmd_list = []
+    for item in location:
+        bucket_type = item[0]
+        bucket_name = item[1]
+        # ceph osd crush add-bucket maroot root
+        cmd_list.append(generate_cmd(cluster, "add-bucket", bucket_name, bucket_type, containerized))
+        if previous_bucket:
+            # ceph osd crush move monrack root=maroot
+            cmd_list.append(generate_cmd(cluster, "move", previous_bucket[1], bucket_type + "=" + bucket_name, containerized))
+        previous_bucket = item
+    return cmd_list
+
+
+def exec_commands(module, cmd_list):
+    '''
+    Creates Ceph commands
+    '''
+    for cmd in cmd_list:
+        rc, out, err = module.run_command(cmd, encoding=None)
+    return rc, cmd, out, err
+
+
+def run_module():
+    module_args = dict(
+        cluster=dict(type='str', required=False, default='ceph'),
+        location=dict(type='dict', required=True),
+        containerized=dict(type='str', required=True, default=None),
+
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    cluster = module.params['cluster']
+    location_dict = module.params['location']
+    location = sort_osd_crush_location(tuple(location_dict.items()), module)
+    containerized = module.params['containerized']
+
+    result = dict(
+        changed=False,
+        stdout='',
+        stderr='',
+        rc='',
+        start='',
+        end='',
+        delta='',
+    )
+
+    if module.check_mode:
+        return result
+
+    startd = datetime.datetime.now()
+
+    # run the Ceph command to add buckets
+    rc, cmd, out, err = exec_commands(module, create_and_move_buckets_list(cluster, location, containerized))
+
+    endd = datetime.datetime.now()
+    delta = endd - startd
+
+    result = dict(
+        cmd=cmd,
+        start=str(startd),
+        end=str(endd),
+        delta=str(delta),
+        rc=rc,
+        stdout=out.rstrip(b"\r\n"),
+        stderr=err.rstrip(b"\r\n"),
+        changed=True,
+    )
+
+    if rc != 0:
+        module.fail_json(msg='non-zero return code', **result)
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
